cc685b06d3dab3646ae59a68d19c5f19cb9036f9
luisf <luisf@Luis>
2025-05-26 20:13:44 -0300

    e

--- projetinho/src/main/java/com/projetinho/projetinho/model/Login.java
+++ projetinho/src/main/java/com/projetinho/projetinho/model/Login.java
@@ -1,5 +1,59 @@
 package com.projetinho.projetinho.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "login")
 public class Login {
 
+    @Id
+    private String login;
+
+    private String senha;
+    private boolean status;
+    private boolean adm;
+
+    public Login() {
+    }
+
+    public Login(String login, String senha, boolean status, boolean adm) {
+        this.login = login;
+        this.senha = senha;
+        this.status = status;
+        this.adm = adm;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean isAdm() {
+        return adm;
+    }
+
+    public void setAdm(boolean adm) {
+        this.adm = adm;
+    }
 }
--- projetinho/src/main/java/com/projetinho/projetinho/model/Treino.java
+++ projetinho/src/main/java/com/projetinho/projetinho/model/Treino.java
@@ -1,5 +1,107 @@
 package com.projetinho.projetinho.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Treino")
 public class Treino {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String musculo;
+    private String exercicio;
+    private Double carga;
+    private int repeticoes;
+    private Double descanso;
+    private String data;
+
+    @ManyToOne
+    @JoinColumn(name = "email", referencedColumnName = "login", insertable = false, updatable = false)
+    private Login login;
+
+    public Treino() {
+    }
+
+    public Treino(String email, String musculo, String exercicio, Double carga, int repeticoes, Double descanso, String data) {
+        this.email = email;
+        this.musculo = musculo;
+        this.exercicio = exercicio;
+        this.carga = carga;
+        this.repeticoes = repeticoes;
+        this.descanso = descanso;
+        this.data = data;
+    }
+
+    // Getters e setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMusculo() {
+        return musculo;
+    }
+
+    public void setMusculo(String musculo) {
+        this.musculo = musculo;
+    }
+
+    public String getExercicio() {
+        return exercicio;
+    }
+
+    public void setExercicio(String exercicio) {
+        this.exercicio = exercicio;
+    }
+
+    public Double getCarga() {
+        return carga;
+    }
+
+    public void setCarga(Double carga) {
+        this.carga = carga;
+    }
+
+    public int getRepeticoes() {
+        return repeticoes;
+    }
+
+    public void setRepeticoes(int repeticoes) {
+        this.repeticoes = repeticoes;
+    }
+
+    public Double getDescanso() {
+        return descanso;
+    }
+
+    public void setDescanso(Double descanso) {
+        this.descanso = descanso;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public Login getLogin() {
+        return login;
+    }
+
+    public void setLogin(Login login) {
+        this.login = login;
+    }
 }
